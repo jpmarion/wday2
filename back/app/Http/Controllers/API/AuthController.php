b3638b5c270e1c561ef137647e23939f8b5c63a9
Juan Pablo Marión <totomarion@gmail.com>
2020-10-02 19:55:27 -0300

    Crear AuthController

--- back/app/Http/Controllers/API/AuthController.php
+++ back/app/Http/Controllers/API/AuthController.php
@@ -3,9 +3,57 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthLoginRequest;
+use App\Http\Requests\AuthSignupRequest;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function signup(AuthSignupRequest $request)
+    {
+        $user = new User([
+            'email' => $request->email,
+            'password' => $request->password,
+            'password_confirmation' => $request->password_confirmation
+        ]);
+        $user->save();
+        return response()->json(['mensaje' => "Usuario creado exitosamente"], 201);
+    }
+
+    public function login(AuthLoginRequest $request)
+    {
+        $credentials = request(['email', 'password']);
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['mensaje' => 'No autorizado'], 401);
+        }
+
+        $user = $request->user();
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->token;
+        if ($request->remember_me) {
+            $token->expires_at = Carbon::now()->addHour(8);
+        }
+        $token->save();
+        return response()->json([
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString()
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        return response()->json([
+            'mensaje' => 'Cerrar sesión correctamente'
+        ]);
+    }
+
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
 }
