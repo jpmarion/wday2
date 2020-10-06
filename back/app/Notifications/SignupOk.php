5a2fad2a4db8137f8235fa433a2d022a9f0d5f94
Juan Pablo Marión <totomarion@gmail.com>
2020-10-06 19:29:16 -0300

    Agregar SignupOk

--- back/app/Notifications/SignupOk.php
+++ back/app/Notifications/SignupOk.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class SignupActivate extends Notification
+class SignupOk extends Notification
 {
     use Queueable;
 
@@ -35,18 +35,13 @@ class SignupActivate extends Notification
     /**
      * Get the mail representation of the notification.
      *
-     * @param  mixed  $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
-    public function toMail($notifiable)
+    public function toMail()
     {
-        $url = url('/api/auth/signup/activate/' . $notifiable->activation_token);
         return (new MailMessage)
-            ->greeting('Confirmación de cuenta')
-            ->subject('Confirma tu cuenta')
-            ->line('Gracias por suscribirte! Antes de continuar, debes configurar tu cuenta.')
-            ->action('Confirmar tu cuenta', url($url))
-            ->line('Muchas gracias por utilizar nuestra aplicación!');
+            ->greeting('Confirmación cuenta')
+            ->line('La cuenta ha sido confirmada con exito.');
     }
 
     /**
